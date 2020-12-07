bb4166afe7b15867c248009c8755d0ea098b29ec
kasjan <kasjan.hinc@gmail.com>
2020-12-07 17:24:59 +0000

    update

--- backend/controllers/league.py
+++ backend/controllers/league.py
@@ -13,8 +13,6 @@ league_schema = LeagueSchema()
 router = Blueprint(__name__, 'league')
 
 
-
-
 @router.route('/leagues', methods=['GET'])
 def index():
 
@@ -23,17 +21,17 @@ def index():
   return league_schema.jsonify(leagues, many=True), 200
 
 
-
 @router.route('/league/<int:id>', methods=['GET'])
 def get_single_league(id):
+
   league = League.query.get(id)
+  print(league)
+  print(id)
 
   if not league:
-    return { 'message': 'League not available' }, 404
+    return {'message': 'League not available'}, 404
 
   return league_schema.jsonify(league), 200
-
-
 
 
 # @router.route('/hotels/notes', methods=['GET'])
@@ -44,8 +42,6 @@ def get_single_league(id):
 #   return populate_note.jsonify(notes, many=True), 200
 
 
-
-
 # @router.route('/league/<int:id>', methods=['GET'])
 # def get_single_league(id):
 #   hotel = Hotels.query.get(id)
@@ -54,7 +50,6 @@ def get_single_league(id):
 #     return { 'message': 'Hotel not available' }, 404
 
 #   return hotels_schema.jsonify(hotel), 200
-
 
 
 # @router.route('/hotels', methods=['POST'])
@@ -76,8 +71,6 @@ def get_single_league(id):
 #   return hotels_schema.jsonify(hotel), 200
 
 
-
-
 # @router.route('/hotels/<int:id>', methods=['PUT'])
 # @secure_route
 # def update_hotel(id):
@@ -92,20 +85,16 @@ def get_single_league(id):
 
 #       partial=True
 #     )
-    
-  # except ValidationError as e:
-  #   return { 'errors': e.messages, 'message': 'Could not update hotel' }
 
-  # if hotel.user != g.current_user:
-  #   return { 'message': 'Unauthorized' }, 401
+    # except ValidationError as e:
+    #   return { 'errors': e.messages, 'message': 'Could not update hotel' }
 
-  # hotel.save()
+    # if hotel.user != g.current_user:
+    #   return { 'message': 'Unauthorized' }, 401
 
-  # return hotels_schema.jsonify(hotel), 201
+    # hotel.save()
 
-
-
-
+    # return hotels_schema.jsonify(hotel), 201
 
 
 # @router.route('/hotels/<int:id>', methods=['DELETE'])
@@ -116,8 +105,6 @@ def get_single_league(id):
 
 #   hotel.remove()
 #   return { 'message': f'Hotel {id}--deleted successfully' }
-
-
 
 
 # @router.route('/hotels/<int:hotel_id>/comments', methods=['POST'])
@@ -136,11 +123,6 @@ def get_single_league(id):
 #   return comment_schema.jsonify(comment)
 
 
-
-
-
-
-
 # @router.route('hotels/<int:hotel_id>/comments/<int:comment_id>', methods=['DELETE'])
 # def comment_delete(hotel_id, comment_id):
 
@@ -153,5 +135,3 @@ def get_single_league(id):
 #   # comment.remove()
 
 #   return { 'message': f'Comment {comment_id}--deleted successfully' }
-
-
--- frontend/components/League.js
+++ frontend/components/League.js
@@ -9,46 +9,39 @@ const League = (props) => {
 
   const id = props.match.params.id
 
-  const [league, setLeague] = useState([])
+  const [league, setLeague] = useState({})
 
   useEffect(() => {
-    axios.get(`https://www.thesportsdb.com/api/v1/json/1/lookupleague.php?id=${id}`)
+    axios.get(`/api/league/${id}`)
       .then(resp => {
-        const league = resp.data.leagues
+        const league = resp.data
         console.log(league)
         setLeague(league)
 
       })
   }, [id])
 
-  console.log('here')
 
 
 
   return <div className="container-custom">
 
-
-
     <div className="league-section">
 
-      {league.map((league, index) => {
-
-        return <div key={index} className="card-league text-center">
-          <img className="card-img-top league-img" src={league.strLogo} alt="Card image cap" />
-          <div className="resfixbtn">
-            <Link to={`/league/${id}/results`} className="btn btn-dark btn-resfix">Results</Link>
-            <Link to={`/league/${id}/fixtures`} className="btn btn-dark btn-resfix">Fixtures</Link>
-          </div>
-          <div className="card-body">
-            <h1 className="year"><strong>Founded: {league.intFormedYear}</strong></h1>
-            <h5 className="card-desc">{league.strDescriptionEN}</h5>
-            <a href={`https://${league.strWebsite}`} target="_blank" className="card-website">LEAGUE WEBSITE</a>
-
-
-          </div>
+      <div className="card-league text-center">
+        <img className="card-img-top league-img" src={league.image} alt="Card image cap" />
+        <div className="resfixbtn">
+          <Link to={`/league/${id}/results`} className="btn btn-dark btn-resfix">Results</Link>
+          <Link to={`/league/${id}/fixtures`} className="btn btn-dark btn-resfix">Fixtures</Link>
         </div>
+        <div className="card-body">
+          <h1 className="year"><strong>Founded: {league.year}</strong></h1>
+          <h5 className="card-desc">{league.description}</h5>
+          <a href={`https://${league.Website}`} target="_blank" className="card-website">LEAGUE WEBSITE</a>
 
-      })}
+        </div>
+      </div>
+
     </div>
   </div>
 
